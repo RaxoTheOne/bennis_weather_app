2228efcdc4cb52a714c27306a304e59a10c33741
RaxoTheOne <traxon.bg@gmail.com>
2024-02-25 01:42:22 +0100

    intl added and change some shit on future

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyWeatherApp());
@@ -13,13 +14,23 @@ class MyWeatherApp extends StatefulWidget {
 
 class _MyWeatherAppState extends State<MyWeatherApp> {
   String _city = 'Berlin'; // Ändere die Stadt hier nach Bedarf
-  String _apiKey = '4802adcc2b0a6db427173599dbaa9d4e'; // Füge hier deinen API-Schlüssel ein
+  String _apiKey =
+      '4802adcc2b0a6db427173599dbaa9d4e'; // Füge hier deinen API-Schlüssel ein
 
   Future<Map<String, dynamic>> getWeatherData() async {
-    String apiUrl =
+    String currentWeatherUrl =
         'https://api.openweathermap.org/data/2.5/weather?q=$_city&appid=$_apiKey&units=metric';
-    http.Response response = await http.get(apiUrl as Uri);
-    return json.decode(response.body);
+    http.Response currentWeatherResponse =
+        await http.get(Uri.parse(currentWeatherUrl));
+    Map<String, dynamic> currentWeatherData =
+        json.decode(currentWeatherResponse.body);
+
+    String forecastUrl =
+        'https://api.openweathermap.org/data/2.5/forecast?q=$_city&appid=$_apiKey&units=metric';
+    http.Response forecastResponse = await http.get(Uri.parse(forecastUrl));
+    Map<String, dynamic> forecastData = json.decode(forecastResponse.body);
+
+    return {'current': currentWeatherData, 'forecast': forecastData};
   }
 
   @override
@@ -30,44 +41,63 @@ class _MyWeatherAppState extends State<MyWeatherApp> {
           title: Text('Wettervorhersage'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FutureBuilder(
-                future: getWeatherData(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    Map<String, dynamic> weatherData = snapshot.data;
-                    return Column(
-                      children: <Widget>[
-                        Text(
-                          'Aktuelles Wetter in $_city:',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '${weatherData['weather'][0]['main']}',
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        Text(
-                          'Temperatur: ${weatherData['main']['temp']}°C',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    );
-                  } else {
-                    return CircularProgressIndicator();
-                  }
-                },
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Vorhersage für die nächsten Tage:',
-                style: TextStyle(fontSize: 20),
-              ),
-              // Hier kannst du die Vorhersage-Widgets hinzufügen
-              // Zum Beispiel: ListView.builder für eine Liste von Vorhersagen
-            ],
+          child: FutureBuilder(
+            future: getWeatherData(),
+            builder: (BuildContext context,
+                AsyncSnapshot<Map<String, dynamic>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                Map<String, dynamic>? weatherData = snapshot.data;
+                if (weatherData == null) {
+                  return Text('No data available');
+                }
+                Map<String, dynamic> currentWeatherData = weatherData['current'];
+                Map<String, dynamic> forecastData = weatherData['forecast'];
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Aktuelles Wetter in $_city:',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      '${currentWeatherData['weather'][0]['main']}',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      'Temperatur: ${currentWeatherData['main']['temp']}°C',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Vorhersage für die nächsten Tage:',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: forecastData['list'].length,
+                        itemBuilder: (BuildContext context, int index) {
+                          Map<String, dynamic> forecast = forecastData['list'][index];
+                          DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(forecast['dt'] * 1000);
+                          String date = DateFormat('dd.MM.yyyy').format(dateTime);
+                          String time = DateFormat('HH:mm').format(dateTime);
+                          String forecastMain = forecast['weather'][0]['main'];
+                          double forecastTemp = forecast['main']['temp'].toDouble();
+                          return ListTile(
+                            title: Text('$date $time: $forecastMain, $forecastTemp°C'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ),
