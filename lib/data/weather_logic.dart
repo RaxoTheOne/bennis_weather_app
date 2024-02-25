fe4c03206bed0733870ed6a358d261cfce56e174
RaxoTheOne <traxon.bg@gmail.com>
2024-02-25 01:09:38 +0100

    changed back is better

--- lib/data/weather_logic.dart
+++ lib/data/weather_logic.dart
@@ -1,16 +1,1 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
-class WeatherLogic {
-  String city;
-  String apiKey;
-
-  WeatherLogic(this.city, this.apiKey);
-
-  Future<Map<String, dynamic>> getWeatherData() async {
-    String apiUrl =
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
-    http.Response response = await http.get(Uri.parse(apiUrl));
-    return json.decode(response.body);
-  }
-}
