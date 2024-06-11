465ea39ed321da19e07a691d66ce7e69c92c4cee
RaxoTheOne <traxon.bg@gmail.com>
2024-06-11 13:30:51 +0200

    updated

--- lib/data/weather_api.dart
+++ lib/data/weather_api.dart
@@ -3,11 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherApi {
-  static Future<Map<String, dynamic>> getWeatherData(String city, String apiKey) async {
+  static Future<Map<String, dynamic>> getWeatherData(
+      String city, String apiKey) async {
     String currentWeatherUrl =
         'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
-    http.Response currentWeatherResponse = await http.get(Uri.parse(currentWeatherUrl));
-    Map<String, dynamic> currentWeatherData = json.decode(currentWeatherResponse.body);
+    http.Response currentWeatherResponse =
+        await http.get(Uri.parse(currentWeatherUrl));
+    Map<String, dynamic> currentWeatherData =
+        json.decode(currentWeatherResponse.body);
 
     String forecastUrl =
         'https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$apiKey&units=metric';
